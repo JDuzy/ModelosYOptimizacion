b7b4e67837af70cff4b90b8a68ded47ea98bee26
Juan Duzac <56564649+JDuzy@users.noreply.github.com>
2021-09-22 21:43:22 -0300

    Add files via upload

--- washingOptimizer.py
+++ washingOptimizer.py
@@ -11,7 +11,7 @@ def main():
 	wash_number = 1
 	washing_time = 0
 	enqueed_clothing= set()
-	#washes = []
+	washes = []
 
 	with open("output.txt", 'w') as output:
 
@@ -20,25 +20,31 @@ def main():
 			if actual_clothing in enqueed_clothing:
 				continue
 
-			#wash = [actual_clothing]
+			wash = {actual_clothing}
 			output.write(actual_clothing + " " + str(wash_number) + "\n")
-			#washing_time += int(oredered_washing_times[actual_clothing])
+			washing_time += int(oredered_washing_times[actual_clothing])
 			enqueed_clothing.add(actual_clothing)	
 
 			for clothing in oredered_washing_times.keys():
-				if is_incompatible(actual_clothing, clothing) or clothing in enqueed_clothing:
+				if is_incompatible(clothing, wash) or clothing in enqueed_clothing:
 					continue
 
-				#wash.append(clothing)
+				wash.add(clothing)
 				output.write(clothing + " " + str(wash_number) + "\n")
 				enqueed_clothing.add(clothing)
 
 			wash_number += 1
-			#washes.append(wash)
+			washes.append(wash)
+
+		print(washing_time)
+		print(washes)	
 
 
-def is_incompatible(cloth1, cloth2):
-	return cloth2 in INCOMPATIBILITIES[cloth1]
+def is_incompatible(cloth1, wash):
+	for clothing in wash:
+		if cloth1 in INCOMPATIBILITIES[clothing]:
+			return True
+	return False
 
 
 main()
